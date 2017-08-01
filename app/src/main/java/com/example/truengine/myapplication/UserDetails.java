cbbc16cc8717f034c3ace1dcf1dbc9f07e85614d
Ryan Foster <fosterdavidryan@gmail.com>
2017-08-01 21:28:19 +0100

    Initial commit

---
+++ app/src/main/java/com/example/truengine/myapplication/UserDetails.java
@@ -0,0 +1,11 @@
+package com.example.truengine.myapplication;
+
+/**
+ * Created by truengine on 7/31/2017.
+ */
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
